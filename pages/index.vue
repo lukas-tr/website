23471233e03b551069ec2119e2dcb8f461bee1f7
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2021-08-11 20:14:09 +0200

    initial

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row class="fill-height" justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <OverviewCard />
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.fill-height {
+  min-height: 100vh;
+}
+</style>
